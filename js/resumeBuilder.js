4f985107cf842f7d7c8eba208bd473e1a156b3a6
Jared Echols <jaredechols@gmail.com>
2015-10-27 22:44:41 -0600

    added basic stuff name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var name = "Jared Echols";
+var role = "Software Developer";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
